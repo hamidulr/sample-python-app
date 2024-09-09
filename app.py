29dde696f70d5a968a2129c9fb74c81d98b3bc40
hamidulr <59969056+hamidulr@users.noreply.github.com>
2024-09-09 10:10:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world to AWS users!'
+    return 'Hello, world to AWS DevOps users!'
 
 if __name__ == '__main__':
     app.run()
